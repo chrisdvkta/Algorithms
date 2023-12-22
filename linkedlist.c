d1d27cc7fb0133fa16949ff502ff1e3aec5fa416
chrisdvkta <chrisdevkota77@gmail.com>
2023-12-22 14:29:40 +0545

    commit_x

--- linkedlist.c
+++ linkedlist.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define NAME_LEN 25
 
 struct node{
@@ -13,11 +14,15 @@ int main(){
 struct node *first = NULL;
 
 
-struct node *new_node; 
-new_node = malloc(sizeof(struct node));
-(*new_node).value = 120; 
-
-new_node->next = first; 
-first = new_node ;
-
+char s[1]; 
+do{
+    printf("Enter : ");
+    scanf(" %c",&s[0]);
+    struct node *new_node; 
+    new_node = malloc(sizeof(struct node)); 
+    scanf(" %d",(*new_node).value);
+    new_node->next = first; 
+    first = new_node ;
+}while(s[0] !='n');
+printf("done");
 }
